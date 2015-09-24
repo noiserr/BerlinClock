0eb0441eac0ba1cd566679deb2658609a4d10075
Mateusz Budzar <mateusz.budzar@droidsonroids.pl>
2015-09-24 10:16:08 +0200

    Initial commit

---
+++ app/src/test/java/pl/droidsonroids/berlinclock/model/BerlinClockTest.java
@@ -0,0 +1,13 @@
+package pl.droidsonroids.berlinclock.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+public class BerlinClockTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
